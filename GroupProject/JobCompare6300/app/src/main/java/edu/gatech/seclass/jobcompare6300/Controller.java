9e05f1fb717653f469a1a73475aa0ec9ebe32260
Valentin Pintea <vpintea3@gatech.edu>
2021-10-14 18:35:49 -0600

    D3 controller implementation (#14)
    * Implement Controller's addLocation method
    
    * Update Controller.java
    
    Co-authored-by: Lacey, Arthur <alacey7@gatech.edu>

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/Controller.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/Controller.java
@@ -1,5 +1,8 @@
 package edu.gatech.seclass.jobcompare6300;
 
+import android.content.Intent;
+import android.view.View;
+
 import java.util.ArrayList;
 
 public class Controller {
@@ -14,40 +17,73 @@ public class Controller {
     }
 
     public void run(){
+        //start DB handler
+
+        /*loop
+        switch(user button selection){
+            case enter current job:
+            case enter job offer:
+            case adjust comparison weights:
+            case compare job offers:
+            case exit:
+                //spin down DB handler
+                //
+            case default:
+                continue
+        */
+
+
         /* listen for menu selection
         handle selection accordingly
         loop
          */
+
+
     }
 
-    public void editCurrentJob(){
+    public void editCurrentJob(View view){
+        //Intent intent = new Intent(this, enterCurrentJobActivity.class);
+        //startActivity(intent);
+
         /* display activity_enter_current_job
         save current job's details in jobOffers if save button is selected, otherwise cancel
         return to main menu
          */
     }
 
-    public void enterJobOffer(){
+    public void enterJobOffer(View view){
         /* display activity_enter_job_offer
         save/cancel
         handle appropriate follow-on action
          */
     }
 
-    public void adjustWeights(){
+    public void adjustWeights(View view){
         /* display activity_adjust_comparison_weights
         save/cancel then return to main menu
          */
     }
 
-    public void compareOffers(){
+    public void compareOffers(View view){
         /* display activity_compare_jobs
 
          */
     }
 
+    //adds location if it does not exist, updates the cost of living index if the location does exist.
     private void addLocation(String city, String state, int colIndex){
-        
+        //O(2n)
+        if(!locationIsStored(city,state)){
+            locations.add(new Location(city, state, colIndex));
+        }
+        else{
+            for (Location l: locations) {
+                if(l.getCity().equals(city) && l.getState().equals(state)){
+                    l.setCostOfLivingIndex(colIndex);
+                    break;
+                }
+            }
+        }
     }
     
     private boolean locationIsStored(String city, String state){
