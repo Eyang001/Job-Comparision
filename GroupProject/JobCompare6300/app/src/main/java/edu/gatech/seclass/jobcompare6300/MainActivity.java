4245cf8a7792b29ed4772b9df76695281f695d47
Valentin Pintea <vpintea3@gatech.edu>
2021-10-14 18:29:59 -0600

    D3 implement main menu layout (#12)
    * Add Initial Structure for Application
    
    * Add main menu buttons
    
    * Add main menu hooks into controller and update button onClick functions
    
    * Add application header
    
    Co-authored-by: Lacey, Arthur <alacey7@gatech.edu>

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/MainActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/MainActivity.java
@@ -2,13 +2,42 @@ package edu.gatech.seclass.jobcompare6300;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Controller controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        controller = new Controller();
+    }
+
+    //https://developer.android.com/training/basics/firstapp/starting-activity
+    public void editCurrentJob(View view){
+        controller.editCurrentJob(view);
+        //Intent intent = new Intent(this, )
+    }
+
+    public void enterJobOffer(View view){
+        controller.enterJobOffer(view);
+    }
+
+    public void adjustComparisonWeights(View view){
+        controller.adjustWeights(view);
+    }
+
+    public void compareJobOffers(View view){
+        controller.compareOffers(view);
+    }
+
+    public void exit(View view){
+        //controller.exit();
+
     }
 }
