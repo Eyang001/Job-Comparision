a2e22086cd378ddb5ab24942b2b8eb3fc6d57caa
Valentin Pintea <vpintea3@gatech.edu>
2021-10-14 18:38:47 -0600

    D3 ey (#15)
    * Add Initial Structure for Application
    
    * Updated jobOffers
    
    Co-authored-by: Lacey, Arthur <alacey7@gatech.edu>
    Co-authored-by: Zhaoran Yang <zyang625@gatech.edu>

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobOffers.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/JobOffers.java
@@ -26,6 +26,12 @@ public class JobOffers {
         update the value in ranked offers accordingly
         call sortJobOffers
          */
+        if !dirtyScores {return};
+        for(Map.Entry<Job, Float> pair : jobOffers.entrySet()){
+            float newScore = calculateScore(pair.getKey(),weights);
+            jobOffers.put(pair.getKey(),newScore);
+        }
+        markScoresClean();
     }
 
     private void sortJobOffers(){
@@ -46,7 +52,23 @@ public class JobOffers {
         return this.jobOffers.get(0);
     }
 
+    //how to differentiate currentJobOffer vs jobOffer?
     public void addOffer(Job job, ComparisonWeights weights){
-
+        jobOffers[0] != null ? jobOffers[jobOffers.size()] = job : jobOffers[jobOffers.size()-1] = job;
+        float score = calculateScore(job, weights);
+        rankedJobOffers.put(job, score);
     }
+
+    public float calculateScore(Job job, ComparisonWeights weights){
+        int yearlySalary = weights.getyearlySalary();
+        int yearlyBonus = weights.getyearlyBonus();
+        int teleDays = weights.getteleDays();
+        int leave = weights.getleaveDays();
+        int gym = weights.getGymAllowance();
+        int sumWeights = yearlySalary + yearlyBonus + teleDays + leave + gym;
+        return job.getSalary() * yearlySalary / sumWeights + job.getBonus() * yearlyBonus / sumWeights
+                + job.getGymAllowance() * gym / sumWeights + (job.getLeaveDays() * job.getSalary() / 260) * leave / sumWeights
+                + ((260 - 52 * job.getTeleworkDays()) * (job.getSalary() / 260) / 8) * teleDays / sumWeights;
+    }
+
 }
