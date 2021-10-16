770703897cae0420b229cc8edce2b3c83a830e8a
Lacey, Arthur <alacey7@gatech.edu>
2021-10-16 04:37:25 -0400

    Add Initial unit/integration testing for controller

---
+++ GroupProject/JobCompare6300/app/src/test/java/edu/gatech/seclass/jobcompare6300/ControllerUnitTest.java
@@ -0,0 +1,44 @@
+package edu.gatech.seclass.jobcompare6300;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ControllerUnitTest {
+    private Controller controller;
+    @Before
+    public void setup(){
+        controller = new Controller();
+    }
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testEditCurrentJob(){
+        //controller does not do input validation;
+        String title = "test title";
+        String company ="test company";
+        String city = "Seattle";
+        String state = "WA";
+        int colIndex = 100;
+        int salary = 200000;
+        int bonus = 50000;
+        int teleworkDays = 0;
+        int leaveDays = 25;
+        int gymAllowance = 500;
+        controller.editCurrentJob(title, company, city, state, colIndex, salary, bonus, teleworkDays, leaveDays, gymAllowance);
+        assertNotNull(controller.getCurrentJob());
+        assertNotNull(controller.getLocationByCityState(city,state));
+        Location location = controller.getLocationByCityState(city, state);
+        assertEquals("Seattle", location.getCity());
+        assertEquals("WA", location.getState());
+        assertEquals(100, location.getCostOfLivingIndex());
+    }
+}
