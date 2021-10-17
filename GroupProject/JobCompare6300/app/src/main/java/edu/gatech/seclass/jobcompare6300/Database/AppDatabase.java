c57f9b33cc19fa97ea6fb4148397b474619121da
Valentino Pintea <vpintea3@gatech.edu>
2021-10-17 10:59:15 -0600

    updated AppDatabase class constructor

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/Database/AppDatabase.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/Database/AppDatabase.java
@@ -6,8 +6,13 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import edu.gatech.seclass.jobcompare6300.ComparisonWeights;
 import edu.gatech.seclass.jobcompare6300.Database.DAO.ComparisonWeightsDAO;
+import edu.gatech.seclass.jobcompare6300.Database.DAO.DirtyScoresDAO;
+import edu.gatech.seclass.jobcompare6300.Database.DAO.JobAndJobOffersDAO;
 import edu.gatech.seclass.jobcompare6300.Database.DAO.JobDAO;
+import edu.gatech.seclass.jobcompare6300.Database.DAO.JobWithLocationDAO;
+import edu.gatech.seclass.jobcompare6300.Database.DAO.LocationDAO;
 import edu.gatech.seclass.jobcompare6300.Database.Entities.ComparisonWeightsEntity;
 import edu.gatech.seclass.jobcompare6300.Database.Entities.JobEntity;
 import edu.gatech.seclass.jobcompare6300.Database.Entities.JobOffersEntity;
@@ -23,26 +28,36 @@ import edu.gatech.seclass.jobcompare6300.Database.Entities.LocationEntity;
 )
 public abstract class AppDatabase extends RoomDatabase {
 
+    public abstract JobDAO userDao();
+    public abstract JobAndJobOffersDAO jobAndJobOffersDAO();
+    public abstract JobWithLocationDAO jobWithLocationDAO();
+    public abstract LocationDAO locationDAO();
+    public abstract ComparisonWeightsDAO comparisonWeightsDAO();
+    public abstract DirtyScoresDAO dirtyScoresDAO();
+
     private static final String DB_NAME = "Job_db";
-    private static volatile AppDatabase INSTANCE;
+    private static AppDatabase INSTANCE;
 
     // Need singleton pattern and call on databasebuilder class b/c we can't instantiate an object of DB
-    static AppDatabase getInstance(Context context) {
+    public static AppDatabase getInstance(Context context) {
         if (INSTANCE == null) {
-            synchronized (AppDatabase.class) {
-                INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                        AppDatabase.class, DB_NAME)
-                        .fallbackToDestructiveMigration()
-                        .build();
-
-            }
+                INSTANCE = buildInstance(context);
         }
         return INSTANCE;
     }
 
+    private static AppDatabase buildInstance(Context context){
+        INSTANCE = Room.databaseBuilder(context,
+                AppDatabase.class, DB_NAME)
+                .allowMainThreadQueries() // ****remove this line when using the APP!!!!!
+                .fallbackToDestructiveMigration()
+                .build();
+        return INSTANCE;
+    }
 
-    public abstract JobDAO userDao();
-    public abstract ComparisonWeightsDAO comparisonWeightsDAO();
+    public void cleanUp(){
+        INSTANCE = null;
+    }
 
 }
 
